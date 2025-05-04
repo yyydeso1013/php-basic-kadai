cb9a09c2da6337eb30a21e53601d9e7c6cee71c7
yyydeso1013 <yyydeso1013@gmail.com>
2025-05-04 23:43:46 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題7</title>
+</head>
+
+<body>
+    <p>
+      <?php
+      //連想配列に値を代入
+      $vegetables_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+      //連想配列の値を出力
+      print_r($vegetables_data);
+      ?>
+    </p>
+</body>
+
+</html>
