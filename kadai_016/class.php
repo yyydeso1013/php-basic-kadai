abec85018828fc0126468b93dca29d06d9ef05a9
yyydeso1013 <yyydeso1013@gmail.com>
2025-05-14 00:50:46 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題16</title>
+</head>
+
+<body>
+    <p>
+      <?php
+      class Food {
+
+        private $name;
+        private $price;
+
+        public function __construct(string $name, int $price) {
+          $this->name = $name;
+          $this->price = $price;
+        }
+        public function show_price() {
+          echo $this->price . '<br>';
+        }
+      }
+
+      class Animal {
+
+        private $name;
+        private $height;
+        private $weight;
+
+        public function __construct(string $name, int $height, int $weight) {
+          $this->name = $name;
+          $this->height = $height;
+          $this->weight = $weight;
+        }
+
+        public function show_height() {
+          echo $this->height;
+        } 
+      }
+      
+      $food = new Food('Potato', 250);
+      $animal = new Animal('dog', 60, 5000);
+      
+      print_r($food);
+       echo "<br>";
+      print_r($animal);
+       echo "<br>";
+
+      $food->show_price();
+      $animal->show_height();
+      ?>
+    </p>  
+</body>
+
+</html>
